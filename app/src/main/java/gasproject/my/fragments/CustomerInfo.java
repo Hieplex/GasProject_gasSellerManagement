1efc01c3cf858ffe3648f8471417460dbbb54f40
Admin <dauma12333@gmail.com>
2022-10-20 08:12:22 +0700

    update

--- app/src/main/java/gasproject/my/fragments/CustomerInfo.java
+++ app/src/main/java/gasproject/my/fragments/CustomerInfo.java
@@ -116,7 +116,6 @@ public class CustomerInfo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_customer_info, container, false);
         TextView textview = view.findViewById(R.id.Name);
         TextView textview2 = view.findViewById(R.id.Phone);
